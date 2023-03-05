3097d91610b1bf841b14f55484477d41b7aeb55e
dika <dika@ciklum.com>
2023-03-05 22:56:27 +0100

    feat: create monorepo libs

---
+++ libs/core/src/lib/core.module.ts
@@ -0,0 +1,8 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {HeaderModule} from "./header";
+
+@NgModule({
+  imports: [CommonModule, HeaderModule],
+})
+export class CoreModule {}
